fc914d6023a05790294761ea678ad90fc28364d9
Kaz Matthews <katherinematthews.polsci@gmail.com>
2017-05-05 09:30:42 -0700

    fix all documents to link to correct layout page. layout page connects successfully to js and css.

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,1 +1,28 @@
-﻿var test = "it works";
+﻿$(document).ready(function () {
+    console.log("hey");
+    $('.edit-form').click(function (event) {
+        $.ajax({
+            type: 'GET',
+            dataType: 'html',
+            url: '/Cupcake/Edit/' + this.value,
+            success: function (result) {
+                $('.return-edit').html(result);
+            }
+        });
+    });
+
+    $('.edit-cupcake').submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            url: 'Cupcake/Edit',
+            type: 'POST',
+            dataType: 'json',
+            data: $(this).serialize(),
+            success: function (result) {
+                var editedCupcake = result.name;
+                var cupcakeId = result.id.toString();
+                $('#' + cupcakeId).text(editedCupcake);
+            }
+        });
+    });
+});
